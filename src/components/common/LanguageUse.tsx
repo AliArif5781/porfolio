38a11a430278892e0d1314618bf6408c4156c34e
AliArif5781 <aliairf92@gmail.com>
2024-12-12 16:03:07 +0500

    Add LanguageUse.tsx

---
+++ src/components/common/LanguageUse.tsx
@@ -0,0 +1,32 @@
+const LanguageUse = () => {
+  return (
+    <div>
+      <div className="flex min-h-0 flex-col gap-y-3 py-2">
+        <div className="flex flex-wrap gap-1">
+          <div className="opacity: 1; filter: blur(0px); transform: translateY(-6px) translateZ(0px);">
+            <div className="inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-[#e1e1e1] text-gray-600">
+              TailwindCSS
+            </div>
+          </div>
+          <div className="opacity: 1; filter: blur(0px); transform: translateY(-6px) translateZ(0px);">
+            <div className="inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent  bg-[#e1e1e1] text-gray-600 ">
+              Typescript
+            </div>
+          </div>
+          <div className="opacity: 1; filter: blur(0px); transform: translateY(-6px) translateZ(0px);">
+            <div className="inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-[#e1e1e1] text-gray-600">
+              React
+            </div>
+          </div>
+          <div className="opacity: 1; filter: blur(0px); transform: translateY(-6px) translateZ(0px);">
+            <div className="inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-[#e1e1e1] text-gray-600">
+              Redux toolkit
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LanguageUse;
